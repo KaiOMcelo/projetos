fe7081f996626ddb02640ead3d604e8dbd9c7e12
KaiOMcelo <kaiomarcelosuporte@gmail.com>
2022-10-11 14:38:09 -0300

    Primeiro Commit, add arquivos no repositorio

---
+++ exemp01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Código em Php</title>
+</head>
+<body>
+    <h1>
+        Título do texto
+    </h1>
+    <p>
+        <?php 
+            echo 'Olá, mundo'; 
+        ?>
+    </p>
+    
+</body>
+</html>
